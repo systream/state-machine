7d365bb17420e04b01221e76246d1e692bc9c58d
Tihanyi Péter <tihi@systream.hu>
2016-06-06 22:02:19 +0200

    Update composer

---
+++ tests/Unit/StateMachine/GenericTransitionTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tihanyipeter
+ * Date: 2016. 06. 06.
+ * Time: 21:53
+ */
+
+namespace Tests\Systream\Unit\StateMachine;
+
+
+class GenericTransitionTest extends \PHPUnit_Framework_TestCase
+{
+
+}
